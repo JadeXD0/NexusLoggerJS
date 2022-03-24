5a82a3ae072c0638f69456c13b1dee71617e46c0
unknown <jadexd.0001@gmail.com>
2022-03-24 21:37:50 +0800

    Adding files

---
+++ NexusLogger.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const chalk = require("chalk");
+const moment = require("moment");
+const fs = require("fs");
+
+const outdatedversions = ["v1", "v1.1", "v1.2", "v1.2.5"];
+const latest = require("./package.json").version;
+
+class NexusLogger {
+    constructor(name) {
+
+        this.name = name;
+        
+        
+        const d = new Date();
+
+        var formatteddate = moment(d).format("h:mm:ss a");
+        let date = formatteddate;
+
+        this.date = date;
+
+    };
+
+    consoleLog(text) {
+        console.log(`${chalk.grey("Log")}  ${chalk.magenta(this.date)} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.whiteBright(text)}`);
+    }
+
+    infoLog(text) {
+        console.log(`${chalk.green("Info")}  ${chalk.magenta(this.date)} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.greenBright(text)}`);
+    };
+
+    warnLog(text) {
+        console.log(`${chalk.yellow("Warn")}  ${chalk.magenta(this.date)} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.yellowBright(text)}`);
+    };
+
+    errorLog(text) {
+        console.log(`${chalk.red("Error")}  ${chalk.magenta(this.date)} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.redBright(text)}`);
+    };
+
+}
+
+async function VersionChecker(version) {
+
+    if(!version.startsWith("v")) version = `v${version}`;
+    if(outdatedversions.includes(`${version}`)) {
+        throw Error(`This version of NexusLoggerJS is outdated! Please update to v${latest}!`);
+    } else {
+        console.log(`Running latest version of NexusLoggerJS! Version: ${chalk.magentaBright(`v${latest}`)} | Current Version: ${chalk.magentaBright(`${version}`)}`);
+    };
+
+};
+
+module.exports = {
+    NexusLogger,
+    VersionChecker
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# What NexusLogger is all About
+
+NexusLogger is a Simple but Advanced Logging System! It features Custom Logging with your own Texts!
+
+## Features already Released and Features that are still being Developed
+
+- [x] Custom Logging
+- [x] Version Checking
+- [ ] File Logging
+- [ ] File Log Saving
+
+### How to use NexusLogger
+
+<p align="center">
+    Example
+</p>
+
+```js
+
+const { NexusLogger } = require("nexusloggerjs");
+const Nexus = new NexusLogger("NexusLogManager") // Just put a name for your console and your ready to go
+
+
+// Example Log
+Nexus.consoleLog("Console Logs");
+
+// Example Info Log
+Nexus.infoLog("An Info Log!");
+
+//Example Warn Log
+Nexus.warnLog("A Warn Log!");
+
+// Example Error Log
+Nexus.errorLog("F*ck.. Why did i have to error!?")
+```
+
+> If you are interested on suggesting we are working on something for suggestions on our discord server!
+> Discord server not availale temporarily
