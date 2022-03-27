996e3acb8110b183a7fd171dc32fed453c92b662
unknown <jadexd.0001@gmail.com>
2022-03-27 21:07:34 +0800

    Fixed version checker my bad

--- NexusLogger.js
+++ NexusLogger.js
@@ -10,6 +10,7 @@ class NexusLogger {
     constructor(name) {
 
         this.name = name;
+        this.version = latest;
         
         const d = new Date();
 
@@ -19,6 +20,18 @@ class NexusLogger {
         this.date = date;
 
     };
+
+    checkVersion(curVer) {
+        if(curVer !== this.version) {
+            throw new Error(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Seems like your version may be outdated, we reccomend you update to our latest version as your current version may not be supported!. Update to the latest version: ${chalk.magenta(this.version)}`)}`);
+        };
+        if(!curVer) {
+            throw TypeError(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Sorry did you define a version your using? We have to check if your using an update version`)}`)
+        }
+        if(curver !== typeof("string")) {
+            throw TypeError(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Is the version you defined even a string? Please make it a string. Example: ${chalk.white(`\`NotNexus.checkVersion("1.5.5")\``)}`)}`);
+        };
+    }
 
     consoleLog(text) {
         console.log(`${chalk.magenta(this.date)} ${chalk.grey("|")} ${chalk.grey("Log")} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.whiteBright(text)}`);
@@ -38,16 +51,6 @@ class NexusLogger {
 
 }
 
-function checkVersion(curVer) {
-    if(curVer !== `${latest}`) {
-        throw new Error(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Seems like your version may be outdated, we reccomend you update to our latest version as your current version may not be supported!. Update to the latest version: ${chalk.magenta(latest)}`)}`);
-    };
-    if(!curVer) {
-        throw TypeError(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Sorry did you define a version your using? We have to check if your using an update version`)}`)
-    }
-}
-
 module.exports = {
-    NexusLogger,
-    checkVersion
+    NexusLogger
 }
