c7d4d1b931858fa14a3897170536546a4230b608
unknown <jadexd.0001@gmail.com>
2022-03-28 11:38:46 +0800

    Fixed file log saver to not include chalk arguments

--- NexusLogger.js
+++ NexusLogger.js
@@ -8,57 +8,70 @@ var util = require('util');
 const latest  = require("./package.json").version;
 
 class NexusLogger {
-    constructor(name, curNum) {
+    constructor(name) {
 
         this.name = name;
         this.version = latest;
         
         const d = new Date();
         const d2 = new Date();
-        curNum = 0;
 
         var formattedtimedate = moment(d).format("h:mm:ss a");
         var formattedDate = moment(d2).format("YYYY-MM-DD");
 
         this.time = formattedtimedate;
         this.date = formattedDate
-        this.logFileNum = curNum;
 
     };
     checkVersion(curVer) {
         if(curVer !== this.version) {
+            const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile.log`, { flags: 'a' })
+
+            fileWrite.write(util.format.apply(null, arguments) + '\n');
             throw new Error(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Seems like your version may be outdated, we reccomend you update to our latest version as your current version may not be supported!. Update to the latest version: ${chalk.magenta(this.version)}`)}`);
         };
 
         if(!curVer) {
+            const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile.log`, { flags: 'a' })
+
+            fileWrite.write(util.format.apply(null, arguments) + '\n');
             throw TypeError(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Sorry did you define a version your using? We have to check if your using an update version`)}`)
         };
 
         if(typeof(curVer) !== typeof(String())) {
+            const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile.log`, { flags: 'a' })
+
+            fileWrite.write(util.format.apply(null, arguments) + '\n');
             throw TypeError(`${chalk.red("Version Error")} ${chalk.grey("|")} ${chalk.whiteBright(`Is the version you defined even a string? Please make it a string. Example: ${chalk.white(`\`NotNexus.checkVersion("1.5.5")\``)}`)}`)
         };
     }
 
     consoleLog(text) {
-        const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile${this.logFileNum++}.log`, { flags: 'a' })
-        var log_stdout = process.stdout;
+        const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile.log`, { flags: 'a' })
 
-        console.log = function() {
-            fileWrite.write(util.format.apply(null, arguments) + '\n');
-        }
-        console.log(`${chalk.magenta(this.time)} ${chalk.grey("|")} ${chalk.grey("Log")} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.whiteBright(text)}`) = console.log;
+        fileWrite.write(util.format.apply(null, arguments) + '\n');
+        console.log(`${chalk.magenta(this.time)} ${chalk.grey("|")} ${chalk.grey("Log")} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.whiteBright(text)}`)
 
     }
 
     infoLog(text) {
+        const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile.log`, { flags: 'a' })
+
+        fileWrite.write(util.format.apply(null, arguments) + '\n');
         console.log(`${chalk.magenta(this.time)} ${chalk.grey("|")} ${chalk.green("Info")} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.greenBright(text)}`);
     };
 
     warnLog(text) {
+        const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile.log`, { flags: 'a' })
+
+        fileWrite.write(util.format.apply(null, arguments) + '\n');
         console.log(`${chalk.magenta(this.time)} ${chalk.grey("|")} ${chalk.yellow("Warn")} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.yellowBright(text)}`);
     };
 
     errorLog(text) {
+        const fileWrite = fs.createWriteStream(`${this.date} NexusLogFile.log`, { flags: 'a' })
+
+        fileWrite.write(util.format.apply(null, arguments) + '\n');
         console.log(`${chalk.magenta(this.time)} ${chalk.grey("|")} ${chalk.red("Error")} ${chalk.magenta("[")}${chalk.magentaBright(this.name)}${chalk.magenta("]")} ${chalk.grey(" | ")} ${chalk.redBright(text)}`);
     };
 
